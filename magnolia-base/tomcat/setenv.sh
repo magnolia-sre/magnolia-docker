e070ca5f255d798a6d07321d1f084c2eaed8e463
Nicolas Barbé <nicolas.barbe@magnolia-cms.com>
2015-02-01 14:29:04 +0100

    Add support for db.address and db.port in setenv.sh

--- magnolia-base/tomcat/setenv.sh
+++ magnolia-base/tomcat/setenv.sh
@@ -20,6 +20,8 @@ export CATALINA_OPTS="$CATALINA_OPTS \
 
 # Database settings 
 export CATALINA_OPTS="$CATALINA_OPTS \
+	-Ddb.address=$DB_ADDRESS \
+	-Ddb.port=$DB_PORT \
 	-Ddb.schema=$DB_SCHEMA \
 	-Ddb.username=$DB_USERNAME \
 	-Ddb.password=$DB_PASSWORD"
