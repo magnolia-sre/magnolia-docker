28de94d1795b870ee0f4c754bef6daf8b3a66b48
Nicolas Barbé <nicolas.barbe@gmail.com>
2015-04-20 14:36:04 +0000

    Symlink are not created if they are already present

--- magnolia-base-debug/tomcat/start-debug.sh
+++ magnolia-base-debug/tomcat/start-debug.sh
@@ -6,9 +6,17 @@ do
 		continue
 	fi
 
-	if [ -d $folder ]; then
-		echo "Adding symbolink link to $folder in WEB-INF/classes"
-		ln -s $folder $CATALINA_BASE/webapps/ROOT/WEB-INF/classes/$(basename $folder)
+	if [ -d $folder ] 
+  then
+
+    symlink=$CATALINA_BASE/webapps/ROOT/WEB-INF/classes/$(basename $folder)
+
+    if [ ! -h $symlink ] 
+    then
+		  echo "Adding symbolink link to $folder in WEB-INF/classes"
+		  ln -s $folder $symlink
+    fi
+
 	fi
 	
 done
