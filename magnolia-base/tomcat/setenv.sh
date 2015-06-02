ec1056592bf21660ba1ebdd2b7bb2cbd1b6b7a04
Nicolas Barbé <nicolas.barbe@magnolia-cms.com>
2015-06-02 10:30:29 +0200

    Generate automatically a cluster ID and provide default values for the database

--- magnolia-base/tomcat/setenv.sh
+++ magnolia-base/tomcat/setenv.sh
@@ -1,28 +1,47 @@
-# Tomcat and JVM Settings - Adjust those default settings according to your needs
+# Container settings - Adjust those default settings according to your needs
 
-# Memory settings
+#
+# JVM settings
+#
+
+export CATALINA_OPTS="$CATALINA_OPTS \
+	-server"
+
+#
+# JVM memory settings
+#
+
 export CATALINA_OPTS="$CATALINA_OPTS \
 	-XX:MaxPermSize=256m \
 	-Xms64M \
 	-Xmx2048M"
 
-# Garbage collector settings
+#
+# JVM garbage collector settings
+#
+
 export CATALINA_OPTS="$CATALINA_OPTS \
 	-XX:+UseParallelGC \
 	-XX:MaxGCPauseMillis=1500 \
 	-XX:GCTimeRatio=9 \
 	-XX:+DisableExplicitGC"
 
-# JVM settings
-export CATALINA_OPTS="$CATALINA_OPTS \
-	-server"
-
-
+#
 # Magnolia Home
+#
+
 export CATALINA_OPTS="$CATALINA_OPTS \
 	-Dmagnolia.home=$MAGNOLIA_HOME"
-	
+
+#	
 # Database settings 
+#
+
+# set some default values
+: ${DB_SCHEMA:=magnolia}
+: ${DB_USERNAME:=magnolia}
+: ${DB_PASSWORD:=magnolia}
+
 export CATALINA_OPTS="$CATALINA_OPTS \
 	-Ddb.address=$DB_ADDRESS \
 	-Ddb.port=$DB_PORT \
@@ -30,17 +49,29 @@ export CATALINA_OPTS="$CATALINA_OPTS \
 	-Ddb.username=$DB_USERNAME \
 	-Ddb.password=$DB_PASSWORD"
 
+#
 # Magnolia cluster Id
+#
+
+# set the cluster ID randomly if not set
+: ${CLUSTER_ID:="cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32"}
+
 export CATALINA_OPTS="$CATALINA_OPTS \
 	-Dorg.apache.jackrabbit.core.cluster.node_id=$CLUSTER_ID"
 
+#
 # JMX Settings:
+#
+
 #export CATALINA_OPTS="$CATALINA_OPTS \
 #	-Dcom.sun.management.jmxremote.port=12345 
 #	-Dcom.sun.management.jmxremote.ssl=false 
 #	-Dcom.sun.management.jmxremote.authenticate=false"
 
+#
 # Debugging
+#
+
 #export CATALINA_OPTS="$CATALINA_OPTS \
 #	-Xdebug 
 #	-Xnoagent 
