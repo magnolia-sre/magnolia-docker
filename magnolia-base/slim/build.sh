e742f0e929c1741d894ec639100c6ea85306f692
Nicolas Barbé <nicolasbarbe@Nicolass-MacBook-Pro.local>
2018-12-04 16:17:46 +0100

    Multi-versions build and move to travis

---
+++ magnolia-base/slim/build.sh
@@ -0,0 +1,13 @@
+echo "# Building Magnolia base images ..."
+
+grep -v '^#' tags.csv | while IFS=, read -r Col1 Col2 
+do
+	# trim spaces
+	export TOMCAT_VER=`echo $Col1`
+	export JDK_VER=`echo $Col2`
+
+    docker build --build-arg TOMCAT_VER  \
+                 --build-arg JDK_VER  \
+                 --tag magnolia-sre/magnolia-base:$TOMCAT_VER-$JDK_VER-slim .
+done
+
