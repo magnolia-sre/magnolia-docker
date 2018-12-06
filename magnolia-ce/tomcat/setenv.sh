d34893806476566a022a6f67180e4512e22cb54a
Nicolas Barbé <nicolasbarbe@Nicolass-MacBook-Pro.local>
2018-12-06 14:39:55 +0100

    Rework dockerfile structure

--- magnolia-ce/tomcat/setenv.sh
+++ magnolia-ce/tomcat/setenv.sh
@@ -15,15 +15,6 @@ export CATALINA_OPTS="$CATALINA_OPTS \
 	-Xms64M \
 	-Xmx2048M"
 
-#	
-# Database settings 
-#
-export CATALINA_OPTS="$CATALINA_OPTS \
-	-Ddb.address=$DB_ADDRESS \
-	-Ddb.port=$DB_PORT \
-	-Ddb.schema=$DB_SCHEMA \
-	-Ddb.username=$DB_USERNAME \
-	-Ddb.password=$DB_PASSWORD"
 
 #
 # Magnolia 
